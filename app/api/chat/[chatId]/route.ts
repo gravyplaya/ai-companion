036cdb44ec7fdc8c2342fa79d21b64b25ae053c6
Geo <gravyplaya@gmail.com>
2024-01-11 21:12:52 -0800

    lib update

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -1,11 +1,10 @@
 import dotenv from "dotenv";
 import { StreamingTextResponse, LangChainStream } from "ai";
 import { auth, currentUser } from "@clerk/nextjs";
-import { Replicate } from "langchain/llms/replicate";
 import { CallbackManager } from "langchain/callbacks";
 import { NextResponse } from "next/server";
 
-import { getUserSubscription } from "@/lib/nocodb";
+// import { getUserSubscription } from "@/lib/nocodb";
 import { MemoryManager } from "@/lib/memory";
 import { rateLimit } from "@/lib/rate-limit";
 import prismadb from "@/lib/prismadb";
